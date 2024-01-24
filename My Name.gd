47f298f815097075da27709a430efa1ff7e8e2ac
Cooper Dean <kreinz.c1@stu.janesville.k12.wi.us>
2024-01-24 13:47:11 -0600

    fadfa
    adsfadsf

--- My Name.gd
+++ My Name.gd
@@ -12,11 +12,12 @@ func _process(delta):
 
 
 func _on_button_pressed():
-		$Label.text="My name is bob."
+		$Label.text="My name is Cooper."
 
 
 func _on_button_2_pressed():
 	$Label.text=""
+
 
 func _on_button_3_pressed():
 	get_tree ().quit ()
