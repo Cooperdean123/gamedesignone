164ad558d4fbb58a51c403c290f4398d90076bed
Cooper Dean <kreinz.c1@stu.janesville.k12.wi.us>
2024-01-29 13:27:15 -0600

    kkkkkkkkkkkkkkkkkkkkkkkkk

--- Prog82a.gd
+++ Prog82a.gd
@@ -6,8 +6,8 @@ func _on_btncalc_pressed():
 	var SpeedLimit = int($txtLimit.text)
 	var CarSpeed = int ($txtSpeed.text)
 var milesover = CarSpeed - SpeedLimit
-var Fine = 20.0 * (milesover * 5.0)
-$lblout.text = "Fine: $%.21" %fine
+var Fine = 20.0 * (milesover * 5.0) 
+$lbl.text = "Fine: $ %.2f" % fine
 
 
 
