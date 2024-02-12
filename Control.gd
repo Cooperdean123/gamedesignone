e764b44f6102f9d55a8d386c003d6e165b0ea8c0
Cooper Dean <kreinz.c1@stu.janesville.k12.wi.us>
2024-02-12 13:30:53 -0600

    rgeqqqqqqqqqqqqq
    gfda

--- Control.gd
+++ Control.gd
@@ -1,7 +1,17 @@
 extends Control
 
 
+func sayhi():
+	print("hi")
 
+func getArea(length, width):
+	var area = length * width
+	return area
 
 func _on_calc_pressed():
-	pass # Replace with function body.
+	sayhi()
+	var l = int($LineEdit.text)
+	var w = int($LineEdit2.text)
+	var a = getArea(l,w)
+	var p = getPerim(l,w) 
+	$lblout.text = "Area: Sd/nPerimiter: sd" % [a, p]
