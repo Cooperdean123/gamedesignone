37a477cafa4dff44227fa5de4ed4aa8201b83d76
Cooper Dean <kreinz.c1@stu.janesville.k12.wi.us>
2024-02-16 13:12:57 -0600

    mitten messe mr

---
+++ bussinessman.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+
+
+func _on_button_pressed():
+	$LineEdit.text = "The Perimiter is 83"
+	
+	
+	
+
+
+func _on_button_2_pressed():
+	$LineEdit.text = ""
+	
+	
+
+
+func _on_button_3_pressed():
